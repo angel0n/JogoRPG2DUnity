b70136e059c1617f2132ccb78178cc3b5d1c7329
angelon <angelon.gabriel@gmail.com>
2022-05-21 22:41:15 -0300

    Crafting correção de bug

--- rpg/Assets/scripts/craft/tree.cs
+++ rpg/Assets/scripts/craft/tree.cs
@@ -7,21 +7,35 @@ public class tree : MonoBehaviour
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
 
+    [SerializeField] private GameObject woodPrefab;
+    [SerializeField] private int qtdTotalWood;
+
+    [SerializeField] private ParticleSystem leafs;
+
+    private bool isCut;
+
     public void OnHit()
     {
         treeHealth --;
         anim.SetTrigger("isHit");
-
+        leafs.Play();
         if(treeHealth <= 0)
         {
+            //cria o drop da arvore com direção aleatoria
+            for (int i = 0; i < qtdTotalWood; i++)
+            {
+                Instantiate(woodPrefab,transform.position + new Vector3(Random.Range(-1f,1f),Random.Range(-1f,1f),0f),transform.rotation);
+            }
             //cria o toco da arvore 
             anim.SetTrigger("cut");
+
+            isCut = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Axe"))
+        if(collision.CompareTag("Axe") && !isCut)
         {
             OnHit();
         }
